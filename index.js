ebdc33bf701a4029ac558c7563e106c11e1b82ce
stephen talley <stvlley.amzn@gmail.com>
2022-02-01 12:44:34 -0600

    commit tree 12b14d42b45658fce0350d3f3f0b6331c1638124

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+// returns the number of blocks given a value
+const distanceInBlocks = Math.abs(42 - someValue)
+    return distanceInBlocks
+}
+distanceFromHqInBlocks(43)
+
+function distanceFromHqInFeet(blocks){
+    return((Math.abs(blocks -42)) * 264)
+    
+}
+distanceFromHqInFeet(43)
+
+function distanceTravelledInFeet(start, end) {
+   return((Math.abs(start - end)) * 264);
+
+}
+// distanceTravelledInFeet(43, 48)
+
+
+
+function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    const distance = distanceTravelledInFeet(start, destination)
+    if(distance <= 400){
+        return 0
+    } else if (distance > 400 && distance <= 2000){
+        return (distance - 400) * .02
+
+    } else if (distance > 2000 && distance < 2500) {
+        return  25
+
+    }else{
+        return "cannot travel that far"
+    }}
+
+
+
+// calculatesFarePrice(start, destination)
